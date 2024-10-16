a76f406f6e6224c892001a091f3ea5e4931151a2
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-10-16 16:54:55 +0700

    materi kelas terakhir

---
+++ app/src/main/java/com/example/playkotlinlayout/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.playkotlinlayout
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.playkotlinlayout.ui.theme.PlayKotlinLayoutTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PlayKotlinLayoutTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BasicCompose(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BasicCompose(
+    modifier: Modifier = Modifier
+){
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "Indonesia",
+            fontSize = 50.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Red,
+            fontFamily = FontFamily.SansSerif
+        )
+
+        Text(text = "0",
+            fontWeight = FontWeight.Bold,
+            fontSize = 25.sp
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.logofifa),
+            contentDescription = null
+        )
+    }
+}
